0b38e9ac5a74c18d6342c5678c5877dcf842a60c
Shrikant-Surwase <surwaseshrikant1729@gmail.com>
2024-05-19 15:15:45 +0530

    config updates

---
+++ config/config.ts
@@ -0,0 +1,7 @@
+const _config = {
+    port: process.env.PORT,
+
+}
+
+export const config = Object.freeze(_config); //make sure freeze property is for readonly mode in js
+
--- server.ts
+++ server.ts
@@ -1,1 +1,9 @@
-console.log('helo')
+import app from "./src/app";
+
+const startServer = () => {
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`Server is started on ${port}`)
+  })
+}
+startServer()
